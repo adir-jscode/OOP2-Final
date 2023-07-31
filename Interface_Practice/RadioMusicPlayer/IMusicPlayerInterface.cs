67385af5e429f89cc7cb1647c6f7dd4a345d76bf
Adir <adir.jscode@gmail.com>
2023-07-31 10:31:09 +0600

    RadioMusic

---
+++ Interface_Practice/RadioMusicPlayer/IMusicPlayerInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioMusicPlayer
+{
+    public interface IMusicPlayerInterface
+    {
+        void Switch(bool on);
+        void Retune(double frequency);
+        void SetVolume(int loudness);
+        void PlayNext();
+        void PlayPrevious();
+    }
+}
