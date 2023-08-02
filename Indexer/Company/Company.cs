083a584c8ed41ea85e65b187dd25b15612dfd6d0
Adir <adir.jscode@gmail.com>
2023-08-02 09:40:21 +0600

    Indexer

---
+++ Indexer/Company/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    internal class Company
+    {
+        private List<Employee> listEmployee;
+
+        public Company()
+        {
+            listEmployee= new List<Employee>();
+            listEmployee.Add(new Employee() { EmployeeID = 1, Name = "John" });
+            listEmployee.Add(new Employee() { EmployeeID = 2, Name = "Marry" });
+            listEmployee.Add(new Employee() { EmployeeID = 3, Name = "Mike" });
+            listEmployee.Add(new Employee() { EmployeeID = 4, Name = "Todd" });
+        }
+
+
+    }
+}
