b78717e7e74a7ff0e0c1a1e5aab1f18b7078d79f
Adir <adir.jscode@gmail.com>
2023-08-03 16:01:36 +0600

    Nullable Type, Coalescing Operator

---
+++ Abstraction/NullableType/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    //main function
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //bool? AreYouMajor = null;
+
+            //if(AreYouMajor == true)
+            //{
+            //    Console.WriteLine("User is a Major");
+            //}
+            //else if(AreYouMajor == false)
+            //{
+            //    Console.WriteLine("User is not a Major");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("User did'nt Ans the question");
+            //}
+
+
+            //ANOTHER EXAMPLE (Null coalecing Operator ??)
+
+            //int AvailableTickets;
+            //int? TicketsOnSale = 100;
+
+            //if(TicketsOnSale == null)
+            //{
+            //    AvailableTickets = 0;
+            //}
+            //else {
+
+            //    AvailableTickets = TicketsOnSale.Value;
+
+            //}
+
+            //Console.WriteLine("Tickets Available = {0}", AvailableTickets);
+
+
+            //Using null coalecing 
+
+            int? TicketsOnSale = 100;
+
+            int AvailableTickets = TicketsOnSale ?? 0;
+
+            Console.WriteLine("Tickets Available : {0} ", AvailableTickets);
+
+        }
+    }
+}
